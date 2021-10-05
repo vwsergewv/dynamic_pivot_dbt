5ab8ea3102c04d41d5d8da423be1777be31f0037
vwsergewv <76942391+vwsergewv@users.noreply.github.com>
2021-10-05 19:05:00 +0200

    Generated by SqlDBM: AdventureWorks - v116 by "serge@sqldbm.com"

---
+++ 10/05/2021 10:04:59 AM/AdventureWorks_Release 1_v116.sql
@@ -0,0 +1,12 @@
+-- *********************** SqlDBM: Snowflake ************************
+-- ******************************************************************
+
+-- ************************************** "Dim_Store"
+CREATE TABLE IF NOT EXISTS "Dim_Store"
+(
+ "Store_Id" integer NOT NULL,
+ CONSTRAINT "PK_685" PRIMARY KEY ( "Store_Id" )
+)
+COMMENT = 'store';
+
+
