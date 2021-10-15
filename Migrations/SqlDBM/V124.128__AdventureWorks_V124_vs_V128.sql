1fa4151f616d78cf753ecdcb915f0c89392fadd2
vwsergewv <76942391+vwsergewv@users.noreply.github.com>
2021-10-15 17:06:35 +0200

    Generated by SqlDBM: AdventureWorks - v128 by "serge@sqldbm.com"

---
+++ Migrations/SqlDBM/V124.128__AdventureWorks_V124_vs_V128.sql
@@ -0,0 +1,11 @@
+-- *********************** SqlDBM: Snowflake ************************
+-- ******************************************************************
+
+ALTER TABLE "Dim_Customer"
+    ALTER COLUMN "Birthdate" COMMENT 'happy birthday!';
+
+ALTER TABLE "Dim_Customer"
+    ALTER COLUMN "Maritalstatus" COMMENT 'S or M';
+
+ALTER TABLE "Edw"."Factfinance"
+    ADD COLUMN "new_col2"  NOT NULL;
