dcb1f45d4e41b5adf998bfe27b7096901e5c7bfe
vwsergewv <76942391+vwsergewv@users.noreply.github.com>
2021-11-19 15:36:48 +0100

    Generated by SqlDBM: AdWorks project - v23 by "serge@sqldbm.com"

---
+++ migrations/SqlDBM/V23__AdWorks_project_V23.sql
@@ -0,0 +1,18 @@
+-- *********************** SqlDBM: Snowflake ************************
+-- ******************************************************************
+
+CREATE FILE FORMAT FileFormatAdventureworksdwEdwFileformat431
+ TYPE = JSON;
+
+
+-- ************************************** Edw.DimCurrency
+CREATE TABLE Edw.DimCurrency
+(
+ CurrencyKey          number(38,0) NOT NULL AUTOINCREMENT START 1 INCREMENT 1 COMMENT 'Currency Identifier',
+ CurrencyAlternateKey varchar(3) NOT NULL COMMENT 'Alternate Currency Identifier',
+ CurrencyName         varchar(50) NOT NULL COMMENT 'Name of the Currency',
+ CONSTRAINT PkDimCurrency PRIMARY KEY ( CurrencyKey )
+)
+COMMENT = 'Dimension for Currency Data';
+
+
